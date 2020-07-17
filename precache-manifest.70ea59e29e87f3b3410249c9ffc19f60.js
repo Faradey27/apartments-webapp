8155fe60e7c80496e2560206ae7bcd0f69c87fe0
Andrii Tiertyshnyi <andrii.tiertyshnyi@gmail.com>
2020-07-17 22:13:35 +0300

    Updates

---
+++ precache-manifest.70ea59e29e87f3b3410249c9ffc19f60.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9641da6e619170aa28ab2778c9086bf9",
+    "url": "/bobw-task/index.html"
+  },
+  {
+    "revision": "a007c85b4ed743a2612f",
+    "url": "/bobw-task/static/css/2.19b7558e.chunk.css"
+  },
+  {
+    "revision": "7051f721579edbccb1da",
+    "url": "/bobw-task/static/css/main.9c81dc95.chunk.css"
+  },
+  {
+    "revision": "a007c85b4ed743a2612f",
+    "url": "/bobw-task/static/js/2.b469c782.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bobw-task/static/js/2.b469c782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7051f721579edbccb1da",
+    "url": "/bobw-task/static/js/main.7408f049.chunk.js"
+  },
+  {
+    "revision": "b7a6dc71ba484da46693",
+    "url": "/bobw-task/static/js/runtime-main.6ea86380.js"
+  },
+  {
+    "revision": "2278fa81542d098361e802f4f27652dc",
+    "url": "/bobw-task/static/media/3d.2278fa81.svg"
+  },
+  {
+    "revision": "d44d45f38187cf267c257ba50ecc6a8d",
+    "url": "/bobw-task/static/media/human.d44d45f3.svg"
+  },
+  {
+    "revision": "f5e06f124561c7cdcd1ba619fc8282eb",
+    "url": "/bobw-task/static/media/logo.f5e06f12.svg"
+  },
+  {
+    "revision": "cc989c95d6cca12ec795493e9f3fa525",
+    "url": "/bobw-task/static/media/photo.cc989c95.jpg"
+  },
+  {
+    "revision": "d9d208a8fdf6b1d9faf33baae414944f",
+    "url": "/bobw-task/static/media/pin.d9d208a8.svg"
+  }
+]);
